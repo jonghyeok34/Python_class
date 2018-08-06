c6ff364e75fd67c7c8da4c2ea13b194c2744c4f2
ãJonghyeok <jonghyeok34@gmail.com>
2018-08-06 12:14:34 +0900

    start

---
+++ python_01/src/180806/first.py
@@ -0,0 +1,15 @@
+# print()함수로 출력
+#문장의 끝은 세미콜론(:) 생략
+#문자열 출력할 떄에는 외따옴표(')나 쌍따옴표(")로 감싼다.
+
+print("Hello world")
+print('python ');print('test')
+
+a = 10
+if a == 10:
+    print('a is 10')
+if a < 11:
+    print('a is smaller than 11')
+else:
+    print('nothing')
+
---
+++ python_01/src/180806/sumtest.py
@@ -0,0 +1,8 @@
+#1부터 10까지 합
+sum=0
+
+for i in range(1,11):
+    sum+=i
+print("sum is",sum,", 인정?")
+
+
