e5ca426ef1da0b25613e2bfaca8e2d3beb8a80e6
ãJonghyeok <jonghyeok34@gmail.com>
2018-08-06 12:40:08 +0900

    start

---
+++ python_01/src/180806/04.variable.py
@@ -0,0 +1,50 @@
+#변수
+#변수명 =값
+
+#정수형
+i=10
+print(i)
+print(type(i))
+
+#실수형
+pi=3.14
+print(pi)
+print(type(pi)) #float
+
+#복소수
+c=3+4j
+print(c)
+a=3
+b=4
+print(complex(3,4))
+d=c.conjugate()
+print(d)
+print(type(c)) #complex
+
+#논리형
+b1=True
+b2=False
+print(b1)
+print(b2)
+print(type(b1))  #bool
+print(type(b2))
+
+#문자형
+s='파이썬'
+print(s)
+print(type(s)) #str
+
+#리스트
+list = [10,20,30,40,50]
+print(list)
+print(type(list)) #list
+
+#튜플
+t=(10,20,30,40,50)
+print(t)
+print(type(t)) #tuple
+
+#딕셔너리
+d={'a':10,'b':20,'c':30}
+print(d)
+print(type(d))
